0d54be2930f4bd2213c1d29006d8c89abb711860
snzk <snzk@snzk-no-MacBook.local>
2012-10-03 23:54:19 +0900

    ちょっとした変更
    tweet内のリンクURLをハイパーリンクとして表示されるように変更。また、"年/月/日 時:分 名前 tweet"の順番で表示されるように変更

--- tw_showtweet.php
+++ tw_showtweet.php
@@ -40,7 +40,7 @@ $rst = mysql_query($sql,$con);
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<!-- CSS デザイン部分	 -->
+	<!-- CSS デザイン部分	開始 -->
 	<style type="text/css">
 		body {
 			margin: 0 0 0 0;
@@ -51,18 +51,9 @@ $rst = mysql_query($sql,$con);
 			background: aqua;
 			border: 7px solid #6699cc;
 		}
-/*
-		#paragraph-1:after{
-			content: ".";
-			height: 0;
-			display: block;
-			clear: both;
-			visibility: hidden;
-		}
-*/
 		sname {
 			font-size: 10px;
-			color: gray;
+			color: #c0c1f4;
 		}
 		time1 {
 			font-size: 10px;
@@ -73,26 +64,38 @@ $rst = mysql_query($sql,$con);
 			color: lime
 		}
 		bunsho {
-			font-size: 12px; 
+			font-size: 11px; 
 			color: white
 		}
 	</style>
+	<!-- CSS デザイン部分 終了 -->
 </head>
 <body>
 	<!-- ここからMySQLに格納したtweetをLOOPで回す -->
 	<?PHP While($col = mysql_fetch_assoc($rst)): ?>
 		<?PHP
-			//DBから読み込んだアイコンのファイル名をテキストファイルに上書き保存する
-			$fp = fopen('tw_iconpath.txt',"w");
-			$imgname = @fwrite($fp,$col['icon'],strlen($col['icon']));
-			fclose($fp);
+			//文字列編集	tweet内にリンクが含まれるとき、aタグで囲みクリックで飛べるようにする
+			//httpで始まるリンクの開始位置、終了位置を検索してURLのみを抽出する
+			$urlstrt = strpos($col['tweet'],'http');
+			if($urlstrt != FALSE){
+				$urlend = strpos($col['tweet'],' ',$urlstrt);
+				if($urlend == FALSE){
+					$url = substr($col['tweet'], $urlstrt);
+				}else{
+					$url = substr($col['tweet'], $urlstrt, $urlend - $urlstrt);
+				}
+				//URLをaタグで囲み、元のtweetのURL部分と差し替える
+				$linkurl = "<a href=\"".$url."\">".$url."</a>";
+				$linktweet = str_replace($url,$linkurl,$col['tweet']);
+			}else{
+				$linktweet = $col['tweet'];
+			}
 		?>
-		<sname><?=$col['screenname']?></sname>
 		<time1><?=$col['year']."/".sprintf("%02d", $col['month'])."/".sprintf("%02d", $col['day']) ?></time1>
-		<time2><?="　".$col['hour'].":".sprintf("%02d", $col['minute']).":".sprintf("%02d", $col['second'])."　" ?></time2>
-		<bunsho><?=$col['tweet']."<br />" ?></bunsho>
-		<tweet id="paragraph-1">
-		</tweet>
+		<time2><?="<a href=\"".'http://www.twitter.com/'.$col['screenname'].'/status/'.$col['tweetid']."\">".
+		$col['hour'].":".sprintf("%02d", $col['minute']).":".sprintf("%02d", $col['second'])."</a>"." " ?></time2>
+		<sname><?=$col['screenname']?></sname>
+		<bunsho><?=$linktweet."<br />" ?></bunsho>
 	<?PHP endwhile; ?>
 	<?PHP 	mysql_close($con); ?>
 </body>
