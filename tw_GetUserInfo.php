1acf3e754bed278283349927e9b96662d470a182
Kazunori Shinozaki <shinozaki@initinc.co.jp>
2014-09-11 16:34:57 +0900

    参照するテーブル名の変更・画像保存先を変更

--- tw_GetUserInfo.php
+++ tw_GetUserInfo.php
@@ -4,16 +4,16 @@
   //そのままのサイズで画像を取得
   $data = file_get_contents($iconurl);
   //userのscreen_nameで画像をサーバーに保存する
-  file_put_contents('/home/users/2/boo.jp-snzk/web/PEAR/PEAR/twIcon/'.$iconname,$data);
+  file_put_contents('/home/snzk/www/MyTwitLogger/images/icon/'.$iconname,$data);
   //ユーザー情報の登録
   //既に登録済みか調べる
-  $sqlusr = "SELECT * FROM `tbl_twUser` WHERE userid = $userid";
+  $sqlusr = "SELECT * FROM `tweets` WHERE userid = $userid";
   $rstusr = mysql_query($sqlusr,$con);
   $rstnum = mysql_num_rows($rstusr);
-  echo "INSERT INTO `tbl_twUser`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')<br />";
+  echo "INSERT INTO `tweets`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')<br />";
   If($rstnum == 0){
     //登録されていなければINSERT文を発行
-    $sqlusr = "INSERT INTO `tbl_twUser`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')";
+    $sqlusr = "INSERT INTO `tweets`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')";
     $rstusr = mysql_query($sqlusr,$con);
     If($sqlusr){
       echo $cname."の情報を登録"."<br />";
