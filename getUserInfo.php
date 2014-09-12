85722237d5c6f7c704e0eaf8cd136d60d1308e2d
Kazunori Shinozaki <shinozaki@initinc.co.jp>
2014-09-12 13:23:10 +0900

    getUserInfo:参照するテーブル名を変更

--- getUserInfo.php
+++ getUserInfo.php
@@ -5,14 +5,14 @@
   $data = file_get_contents($iconurl);
   file_put_contents('/home/users/2/boo.jp-snzk/web/PEAR/PEAR/twIcon/'.$iconname,$data);
 
-  $sqlusr = "SELECT * FROM `tbl_twUser` WHERE userid = $userid";
+  $sqlusr = "SELECT * FROM `users` WHERE userid = $userid";
   $rstusr = mysql_query($sqlusr,$con);
   $rstnum = mysql_num_rows($rstusr);
 
-  echo "INSERT INTO `tbl_twUser`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')<br />";
+  echo "INSERT INTO `users`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')<br />";
 
   If($rstnum == 0){
-    $sqlusr = "INSERT INTO `tbl_twUser`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')";
+    $sqlusr = "INSERT INTO `users`(`userid`, `screenname`, `name`, `icon`) VALUES ($userid,$sname,$name,'$iconname')";
     $rstusr = mysql_query($sqlusr,$con);
     If($sqlusr){
       echo $cname."の情報を登録"."<br />";
